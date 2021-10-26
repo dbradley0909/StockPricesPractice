ffafb6c6f52efcc5beffe4d53c2f975e88f0bd00
Dana Bradley <dbradley0909@outlook.com>
2021-10-26 13:52:11 -0400

    first commit

---
+++ stockPrices.js
@@ -0,0 +1,21 @@
+function best(prices,newBuyPrice,buyPrice ){
+   let buyPrice = 0;
+   if(prices.length < 2){
+      let buyPrice = 0;
+      for (let i = 0; i < prices.length-1; i++)
+         for (let t = 0; t < prices.length; t++)
+   } else if(i < t)
+          let newBuyPrice = prices[i] - prices[j]
+        } else if { newBuyPrice > buyPrice)
+         lnewBuyPrice = buyPrice
+   
+          }
+         }
+   }
+   return buyPrice;
+}
+
+   
+   
+
+ console.log(best([1, 2, 3, 4, 5]));
